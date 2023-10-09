bd6b8d94c5dd9188ed12a5b1fb6943c6cb59449f
JoaoMesa <navesmesajoao@gmail.com>
2023-10-09 20:59:27 -0300

    Testing

--- src/testing.c
+++ src/testing.c
@@ -2,6 +2,16 @@
 #include "SortingAlgorithms/Sorting.h"
 
 int main(){
-
-    return 0;
+	int vetorselection[10];
+	for(int i = 0; i < 10; i++){
+		vetorselection[i] = 9-i;
+	}
+	
+	printf("Vetor selection: ");
+	selectionsort(vetorselection, 10);
+	for(int i = 0; i < 10; i++){
+		printf("%d ", vetorselection[i]);
+	}
+	
+	return 0;
 }
