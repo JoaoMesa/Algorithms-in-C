f4105783282dca0c04c4486cccb9905945c463ea
JoaoMesa <navesmesajoao@gmail.com>
2023-10-13 12:01:14 -0300

    Testing update

--- src/testing.c
+++ src/testing.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include "SortingAlgorithms/Sorting.h"
+#define SIZE 10
+
+void fillvector(int vector[]){
+	for(int i = 0; i < SIZE; i++){
+		vector[i] = 9-i;
+	}
+}
+
+void printvector(int vector[]){
+	for(int i = 0; i < SIZE; i++){
+		printf("%d ", vector[i]);
+	}
+}
+
 
 int main(){
-	int vetorselection[10];
-	for(int i = 0; i < 10; i++){
-		vetorselection[i] = 9-i;
-	}
+	int vetorselection[SIZE];
+	fillvector(vetorselection);
 	
-	printf("Vetor selection: ");
-	selectionsort(vetorselection, 10);
-	for(int i = 0; i < 10; i++){
-		printf("%d ", vetorselection[i]);
-	}
+	selectionsort(vetorselection, SIZE);
+
+	printvector(vetorselection);
 	
 	return 0;
 }
+
